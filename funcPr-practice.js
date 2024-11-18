ef88627a15c19a512f641b6a835016db87346001
eric91405 <70065690+eric91405@users.noreply.github.com>
2024-11-18 17:51:41 +0900

    Add files via upload

---
+++ funcPr-practice.js
@@ -0,0 +1,40 @@
+const cart = [
+    { item: "노트북", price: 1200000, quantity: 1 },
+    { item: "마우스", price: 35000, quantity: 2 },
+    { item: "키보드", price: 89000, quantity: 1 }
+];
+let totalPrice = 0;
+for (let i=0; i<cart.length; i++)
+{
+    totalPrice +=cart[i].price*cart[i].quantity;
+}
+console.log(`TotalPrice: ${totalPrice}`);
+
+totalPrice=0;
+
+cart.forEach(goods=> {
+        totalPrice +=goods.price*goods.quantity;
+    }
+)
+console.log(`TotalPrice: ${totalPrice}`);
+
+totalPrice=cart.reduce((sum,goods)=>sum+goods.price*goods.quantity,0);
+console.log(`TotalPrice: ${totalPrice}`);
+
+const itemTotals=cart.map(
+    goods=> ({
+        item: goods.item, total: goods.price*goods.quantity
+    })
+);
+console.log('Total price:', itemTotals);
+
+const names = ['alice', 'bob', 'charlie'];
+const uppercasedNames = names.map(
+    names=>names.toUpperCase()
+);
+console.log(`upper cased names: ${uppercasedNames}`);
+
+const capitalStartnames = names.map(
+    names=>names[0].toUpperCase()+names.slice(1)
+);
+console.log(`capital start names: ${capitalStartnames}`);
